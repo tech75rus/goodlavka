205cb86e88b5b13c08e973fdd7d44b35c514841d
tech75rus <tech75rus@mail.ru>
2022-05-24 05:44:09 +0900

    наброски по платежной системе

---
+++ api/src/Controller/Shop/PayController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller\Shop;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Uid\Uuid;
+use YooKassa\Client;
+
+class PayController extends AbstractController
+{
+    private $testEnv;
+    private $database;
+
+    public function __construct($testEnv, $database)
+    {
+        $this->testEnv = $testEnv;
+        $this->database = $database;
+    }
+
+//    #[Route('/shop/test-pay')]
+//    public function testPay(): Response
+//    {
+//        dd($this->testEnv, $this->database);
+//        $client = new Client();
+//        $client->setAuth('877284', 'test_BOCe1av5wyDo7qQF6qpmp1z6QabCijx3CAc2bWCxJgk');
+//        $key = uniqid('', true);
+//        $key = Uuid::v4();
+//        $response = $client->createPayment(
+//            [
+//                'amount' => [
+//                    'value' => '998.34',
+//                    'currency' => 'RUB',
+//                ],
+//                'payment_method_data' => [
+//                    'type' => 'bank_card',
+//                ],
+//                'confirmation' => [
+//                    'type' => 'redirect',
+//                    'return_url' => 'https://goodlavka.com',
+//                ],
+//                'capture' => true,
+//                'description' => 'Заказ №53',
+//            ],
+//            $key
+//        );
+//        $result = $response->getConfirmation()->getConfirmationUrl();
+//        dd($response);
+//        return new Response($this->testEnv . ' ' . $this->database);
+//    }
+}
